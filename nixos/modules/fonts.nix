12eb2a8a57d7308ef8c42b1802869333cb6e7275
rishabh181104 <rishabhhaldiya18@gmail.com>
2025-07-02 01:32:02 +0530

    s

--- nixos/modules/fonts.nix
+++ nixos/modules/fonts.nix
@@ -2,7 +2,8 @@
 {
   fonts.packages = with pkgs; [
     meslo-lgs-nf
-    font-awesome
-    nerd-fonts.dejavu-sans-mono
+      font-awesome
+      nerd-fonts.dejavu-sans-mono
+      noto-fonts-emoji
   ];
 } 
--- nixos/modules/theming.nix
+++ nixos/modules/theming.nix
@@ -1,7 +1,40 @@
 { config, pkgs, theme, ... }:
 {
   programs.stylix.enable = true;
-  # Example: set a default theme (customize as needed)
+# Example: set a default theme (customize as needed)
+  stylix.targets = {
+    alacritty.enable = true;
+    kitty.enable = true;
+    foot.enable = true;
+    waybar.enable = true;
+    gtk.enable = true;
+    bat.enable = true;
+    btop.enable = true;
+    dunst.enable = true;
+    rofi.enable = true;
+    zathura.enable = true;
+    qt.enable = true;
+  };
+
+  stylix.fonts = {
+    serif = {
+      package = pkgs.dejavu_fonts;
+      name = "DejaVu Serif";
+    };
+    sansSerif = {
+      package = pkgs.dejavu_fonts;
+      name = "DejaVu Sans";
+    };
+    monospace = {
+      package = pkgs.nerdfonts.override { fonts = [ "Meslo" ]; }; # Meslo LG S from Nerd Fonts
+        name = "MesloLGS Nerd Font Mono";
+    };
+    emoji = {
+      package = pkgs.noto-fonts-emoji;
+      name = "Noto Color Emoji";
+    };
+  };
+
   stylix.image = theme.image;
   stylix.base16Scheme = theme.base16Scheme;
 } 
