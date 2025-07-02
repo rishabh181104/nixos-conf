439fcf509d065ef65dd69c1a6ce9add1d49f8fbe
username@lodu.com <username@lodu.com>
2025-07-02 23:55:02 +0530

    s

--- modules/services.nix
+++ modules/services.nix
@@ -1,17 +1,17 @@
 { config, pkgs, lib, ... }:
 {
-  security.polkit.enable = lib.mkForce true;
-  boot.loader.systemd-boot.enable = lib.mkForce true;
+  security.polkit.enable = lib.mkDefault true;
+  boot.loader.systemd-boot.enable = lib.mkDefault true;
   boot.loader.systemd-boot.configurationLimit = 8;
-  boot.loader.efi.canTouchEfiVariables = lib.mkForce true;
+  boot.loader.efi.canTouchEfiVariables = lib.mkDefault true;
   nix.gc = {
-    automatic = lib.mkForce true;
+    automatic = lib.mkDefault true;
     dates = "weekly";
     options = "--delete-older-than 8";
   };
-  boot.initrd.availableKernelModules = lib.mkForce [ "xhci_pci" "ahci" "usb_storage" "sd_mod" "rtsx_pci_sdmmc" ];
-  boot.initrd.kernelModules = lib.mkForce [ ];
-  boot.kernelModules = lib.mkForce [ "kvm-intel" "iwlwifi" ];
+  boot.initrd.availableKernelModules = lib.mkDefault [ "xhci_pci" "ahci" "usb_storage" "sd_mod" "rtsx_pci_sdmmc" ];
+  boot.initrd.kernelModules = lib.mkDefault [ ];
+  boot.kernelModules = lib.mkDefault [ "kvm-intel" "iwlwifi" ];
   boot.extraModulePackages = [ ];
 
 
@@ -19,28 +19,28 @@
 # (the default) this is the recommended approach. When using systemd-networkd it's
 # still possible to use this option, but it's recommended to use it in conjunction
 # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
-  networking.useDHCP = lib.mkForce true;
-# networking.interfaces.enp0s31f6.useDHCP = lib.mkForce true;
-# networking.interfaces.wlp0s20f3.useDHCP = lib.mkForce true;
+  networking.useDHCP = lib.mkDefault true;
+# networking.interfaces.enp0s31f6.useDHCP = lib.mkDefault true;
+# networking.interfaces.wlp0s20f3.useDHCP = lib.mkDefault true;
 
-  nixpkgs.hostPlatform = lib.mkForce "x86_64-linux";
-  hardware.cpu.intel.updateMicrocode = lib.mkForce config.hardware.enableRedistributableFirmware;
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
   services.displayManager.sddm = {
     enable = lib.mkForce true;
-    wayland.enable = lib.mkForce true;
+    wayland.enable = lib.Force true;
   };
 
-  services.picom.enable = lib.mkForce true;
+  services.picom.enable = lib.mkDefault true;
   services.pipewire = {
-    enable = lib.mkForce true;
-    pulse.enable = lib.mkForce true;
+    enable = lib.mkDefault true;
+    pulse.enable = lib.mkDefault true;
   };
 
-  services.libinput.enable = lib.mkForce true;
-  services.openssh.enable = lib.mkForce true;
-  programs.firefox.enable = lib.mkForce true;
-  programs.ssh.startAgent = lib.mkForce true;
-  nixpkgs.config.allowUnfree = lib.mkForce true;
+  services.libinput.enable = lib.mkDefault true;
+  services.openssh.enable = lib.mkDefault true;
+  programs.firefox.enable = lib.mkDefault true;
+  programs.ssh.startAgent = lib.mkDefault true;
+  nixpkgs.config.allowUnfree = lib.mkDefault true;
 
   boot.bootspec.extensions = {
     "org.secureboot.osRelease" = config.environment.etc."os-release".source;
